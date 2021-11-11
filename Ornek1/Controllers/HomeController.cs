a87353e6202e462f98c4f5ee74a89edb879724d6
e-mod <e-mod@DESKTOP-E9LRMFA>
2021-11-11 15:27:20 +0300

    Add project files.

---
+++ Ornek1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Ornek1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ornek1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Kategori> kategoriler = new List<Kategori>
+            {
+                new Kategori{ ID=1,KategoriAdi="Yiyecekler",Aciklama="Yemek yemek güzeldir."},
+                new Kategori{ ID=2,KategoriAdi="İçecekler",Aciklama="Soğuk içiniz."},
+                new Kategori{ ID=3,KategoriAdi="Sebzeler",Aciklama="Faydalıdır."},
+                new Kategori{ ID=4,KategoriAdi="Meyveler",Aciklama="Mandalina kış meyvesidir."},
+                new Kategori{ ID=5,KategoriAdi="Kuruyemişler",Aciklama="Fındık fıstık oooo :)"}
+            };
+            ViewBag.KategoriListesi = kategoriler;
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ Ornek1/Models/Kategori.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ornek1.Models
+{
+    public class Kategori
+    {
+        public int ID { get; set; }
+        public string KategoriAdi { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
